8c475b609ddd3b5c36eb8b6f9b014abb0110f92a
rafaeliannini <ianninirafael@gmail.com>
2024-08-13 16:43:48 -0300

    Inicio

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.texto', {
+    strings:["Estudante de Programação", "Frontend Developer", "Web Developer"],
+    typeSpeed:100,
+    backSpeed:80,
+    backDelay:1000,
+    loop:true
+});
